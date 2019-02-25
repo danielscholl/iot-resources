c676d5eda5a210dbaa73ba9d50b5b719aa458570
Daniel Scholl <daniel@scholllopez.com>
2019-02-25 10:33:16 -0600

    Added aci deploy option to create script

--- aci/README.md
+++ aci/README.md
@@ -1,6 +1,6 @@
 ## Deploying to ACI
 
 ```bash
-az container create --resource-group ${GROUP} --file deploy-$DEVICE.yaml -oyaml
-az container delete --resource-group ${GROUP} --name $DEVICE --yes -oyaml
+az container create --resource-group ${DPS_GROUP} --file deploy-$DEVICE.yaml -oyaml
+az container delete --resource-group ${DPS_GROUP} --name $DEVICE --yes -oyaml
 ```
--- device-cert.sh
+++ device-cert.sh
@@ -17,6 +17,7 @@ function aci_deploy() {
 
   b64_cert=$(openssl base64 -in ./src/pki/certs/${1}-chain.cert.pem |tr -d '\n')
   b64_key=$(openssl base64 -in ./src/pki/private/${1}.key.pem |tr -d '\n')
+  idScope=$(az iot dps list --resource-group $DPS_GROUP --query [0].properties.idScope -otsv)
 
   cat > ./aci/deploy-${1}.yaml << EOF
 apiVersion: '2018-06-01'
@@ -29,8 +30,10 @@ properties:
       environmentVariables:
         - name: 'DEVICE'
           value: '${1}'
-        - name: 'DEVICE_CONNECTION_STRING'
-          secureValue: 'HostName=$HUB.azure-devices.net;DeviceId=$1;x509=true'
+        - name: 'DPS_HOST'
+          value: 'global.azure-devices-provisioning.net'
+        - name: 'ID_SCOPE'
+          value: '${idScope}'
       image: danielscholl/iot-device-js:latest
       ports: []
       resources:
@@ -83,8 +86,8 @@ function save_vault()
 
 function generate_device_certificate()
 {
-  if [ $# -ne 1 ]; then
-    echo "Usage: <subjectName>"
+  if [ $# -lt 1 ]; then
+    echo "Usage: device <deviceName>"
     exit 1
   fi
 
@@ -100,6 +103,13 @@ function generate_device_certificate()
     -oyaml
 
     aci_deploy ${1}
+
+    if [ ${2}="deploy" ]; then
+      printf "\n"
+      tput setaf 2; echo "Deploying Device to ACI" ; tput sgr0
+      tput setaf 3; echo "--------------------------" ; tput sgr0
+      az container create --resource-group ${DPS_GROUP} --file aci/deploy-${1}.yaml -oyaml
+    fi
 }
 
 function generate_edge_certificate()
@@ -162,7 +172,7 @@ function generate_self_certificate()
 
 
 if [ ${1} == "device" ]; then
-    generate_device_certificate ${2}
+    generate_device_certificate ${2} ${3}
 elif [ ${1} == "edge" ]; then
     generate_edge_certificate ${2}
 elif [ ${1} == "leaf" ]; then
